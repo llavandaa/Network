b074ae0c93c54d25ec725fb6d4fe6b6dbad91020
llavandaa <cl.enot@ya.ru>
2025-02-09 00:57:26 +0300

    Update HMAC_client.py

--- Python Crypto/HMAC_client.py
+++ Python Crypto/HMAC_client.py
@@ -22,7 +22,7 @@ def gen_sha256_hmac(message, key):
 async def try_auth(uri):
     async with websockets.connect(uri) as websocket:
         shared_key = "supersecret"
-        message = "Hello from Fuat"
+        message = f"Hello from {input("write ur name")}"
 
         welcome_message = await websocket.recv()
         print(f"Server answered: {welcome_message}")
