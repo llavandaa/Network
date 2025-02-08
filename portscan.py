2c16662e4cca07897fcdf25efaee502d8bd5c492
llavandaa <cl.enot@ya.ru>
2025-02-09 00:16:22 +0300

    Add files via upload
    Uploading scripts

---
+++ portscan.py
@@ -0,0 +1,25 @@
+from scapy.all import IP, TCP, sr1
+
+# Получаем список IP-адресов
+ip_list = [ip.strip() for ip in input("Enter target IPs (comma-separated): ").split(',')]
+
+# Получаем список портов
+ports = list(map(int, input("Enter ports (comma-separated): ").split(',')))
+
+for ip in ip_list:
+    print(f"\nScanning {ip}:")
+    for port in ports:
+        packet = IP(dst=ip)/TCP(dport=port, flags="S")# Создаем SYN-пакет
+        
+        response = sr1(packet, timeout=2, verbose=0)        # Отправляем пакет и получаем ответ
+        
+        # Анализ ответа
+        if response and response.haslayer(TCP):
+            if response[TCP].flags & 0x12:  # SYN-ACK (0x12 = 18)
+                print(f"[+] Port {port:5} → OPEN")
+            elif response[TCP].flags & 0x04:  # RST (0x04 = 4)
+                print(f"[-] Port {port:5} → CLOSED")
+            else:
+                print(f"[?] Port {port:5} → UNEXPECTED FLAGS: {response[TCP].flags}")
+        else:
+            print(f"[!] Port {port:5} → NO RESPONSE")
